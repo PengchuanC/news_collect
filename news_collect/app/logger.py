9ec0f7891845a829afb8e55b08b7dfaa3ce1fd28
pcc <pengchuanchao@outlook.com>
2019-10-10 15:19:24 +0800

    init commit

--- news_collect/app/logger.py
+++ news_collect/app/logger.py
@@ -3,5 +3,5 @@ import logging
 LOG_PATH = "news_collect.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 DATE_FORMAT = "%Y/%m/%d %H:%M"
-logging.basicConfig(filename=LOG_PATH, level=logging.WARNING, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+logging.basicConfig(filename=LOG_PATH, level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
 logs = logging.getLogger(__name__)
