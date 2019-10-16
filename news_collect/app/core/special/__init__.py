73c4a4b89cacd7d03b38b465fea0c347f6407bac
pcc <pengchuanchao@outlook.com>
2019-10-16 09:48:30 +0800

    init commit

--- news_collect/app/core/special/__init__.py
+++ news_collect/app/core/special/__init__.py
@@ -1,2 +1,3 @@
 from app.core.special.specialcollcetor import SpecialCollector
 from app.core.special.specialeastmoney import SpecialEastMoney, SpecialEastMoneySearchApi
+from app.core.special.specialhibor import SpecialHiBor
