45bc73d749aab32e0283ff27267665ed24a7c81e
pcc <pengchuanchao@outlook.com>
2019-10-21 14:32:26 +0800

    去掉无用打印

--- news_collect/app/core/special/specialhibor.py
+++ news_collect/app/core/special/specialhibor.py
@@ -67,6 +67,7 @@ class SpecialHiBor(SpecialCollector):
             return
         content = etree.HTML(content)
         article = content.cssselect("#table1 >  tr")
+        print(content)
 
         news_collections = []
         for a in article:
