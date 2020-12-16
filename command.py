d4feb689a0dcc4f07c3bbc546f2d3061e119cf5e
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2020-12-16 10:21:47 +0800

    添加超时退出设置

--- command.py
+++ command.py
@@ -1,5 +1,7 @@
 import click
+import os
 
+from concurrent.futures import ThreadPoolExecutor, wait
 from app.scheduler import schedule, schedule_special, schedule_special_hibor, schedule_special_search_api
 from app.config import website
 
@@ -7,6 +9,14 @@ from app.config import website
 @click.group()
 def run():
     pass
+
+
+def normal():
+    for name in website.keys():
+        try:
+            schedule(name)
+        except Exception as e:
+            print(e)
 
 
 @run.command()
@@ -28,6 +38,17 @@ def collect_all():
         schedule_special_hibor()
     except Exception as e:
         print(e)
+
+
+@run.command()
+def collect_all_async():
+    pool = ThreadPoolExecutor(4)
+    tasks = []
+    for t in (normal, schedule_special, schedule_special_hibor, schedule_special_search_api):
+        thread = pool.submit(t)
+        tasks.append(thread)
+    wait(tasks, 60*20)
+    os._exit(0)
 
 
 if __name__ == '__main__':
